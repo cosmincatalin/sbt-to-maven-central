d8f2ded8117c08522800d22083101681a7559c16
Cosmin Catalin Sanda <cosmincatalin@gmail.com>
2019-09-11 11:05:28 +0200

    Hardcode the root user to use

--- Dockerfile
+++ Dockerfile
@@ -5,11 +5,10 @@ LABEL "com.github.actions.description"="Publish libraries to Maven Central using
 LABEL "com.github.actions.icon"="book"
 LABEL "com.github.actions.color"="gray-dark"
 
-LABEL version="1.0.4"
+LABEL version="1.0.5"
 LABEL repository="https://github.com/cosmincatalin/sbt-to-maven-central"
 LABEL homepage="https://github.com/cosmincatalin/sbt-to-maven-central"
 LABEL maintainer="Cosmin Catalin Sanda <cosmincatalini@gmail.com>"
 
-USER root
 ADD entrypoint.sh /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-mkdir -p ~/.sbt/1.0/plugins/
+mkdir -p /github/home/.sbt/1.0/plugins/
 
-touch ~/.sbt/1.0/sonatype.sbt
-cat /dev/null > ~/.sbt/1.0/sonatype.sbt
+touch /github/home/.sbt/1.0/sonatype.sbt
+cat /dev/null > /github/home/.sbt/1.0/sonatype.sbt
 {
   echo 'credentials += Credentials("Sonatype Nexus Repository Manager",';
   echo '        "oss.sonatype.org",';
   echo '        "'"$NEXUS_USER"'",'
   echo '        "'"$NEXUS_PASSWORD"'")'
-} >> ~/.sbt/1.0/sonatype.sbt
-sha1sum ~/.sbt/1.0/sonatype.sbt
+} >> /github/home/.sbt/1.0/sonatype.sbt
+sha1sum /github/home/.sbt/1.0/sonatype.sbt
 
-touch ~/.private.key
-cat /dev/null > ~/.private.key
-echo -e "$GPG_PK" > ~/.private.key
-gpg --import --batch ~/.private.key
-rm -rf ~/.private.key
+touch /github/home/.private.key
+cat /dev/null > /github/home/.private.key
+echo -e "$GPG_PK" > /github/home/.private.key
+gpg --import --batch /github/home/.private.key
+rm -rf /github/home/.private.key
 
-touch ~/.sbt/1.0/plugins/plugins.sbt
-cat /dev/null > ~/.sbt/1.0/plugins/plugins.sbt
+touch /github/home/.sbt/1.0/plugins/plugins.sbt
+cat /dev/null > /github/home/.sbt/1.0/plugins/plugins.sbt
 {
   echo 'addSbtPlugin("org.xerial.sbt" % "sbt-sonatype" % "2.5")'
-} >> ~/.sbt/1.0/plugins/plugins.sbt
+} >> /github/home/.sbt/1.0/plugins/plugins.sbt
 
 sbt +publish
