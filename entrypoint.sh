232016871c451680a40fb4ffb2d98cbe32b42802
Cosmin Catalin Sanda <cosmincatalin@gmail.com>
2019-11-18 18:45:18 +0100

    no message

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,15 +2,35 @@
 
 mkdir -p /github/home/.sbt/1.0/plugins/
 
+touch /github/home/.sbt/.credentials
+cat /dev/null > /github/home/.sbt/.credentials
+{
+  echo 'realm=Sonatype Nexus Repository Manager';
+  echo "host=oss.sonatype.org";
+  echo "user=$NEXUS_USER";
+  echo "password=$NEXUS_PASSWORD"
+} >> /github/home/.sbt/.credentials
+sha1sum /github/home/.sbt/.credentials
+
 touch /github/home/.sbt/1.0/sonatype.sbt
 cat /dev/null > /github/home/.sbt/1.0/sonatype.sbt
 {
   echo 'credentials += Credentials("Sonatype Nexus Repository Manager",';
   echo '        "oss.sonatype.org",';
-  echo '        "'"$NEXUS_USER"'",'
+  echo '        "'"$NEXUS_USER"'",';
   echo '        "'"$NEXUS_PASSWORD"'")'
 } >> /github/home/.sbt/1.0/sonatype.sbt
 sha1sum /github/home/.sbt/1.0/sonatype.sbt
+
+touch /github/home/.sbt/1.0/global.sbt
+cat /dev/null > /github/home/.sbt/1.0/global.sbt
+{
+  echo 'credentials += Credentials("Sonatype Nexus Repository Manager",';
+  echo '        "oss.sonatype.org",';
+  echo '        "'"$NEXUS_USER"'",';
+  echo '        "'"$NEXUS_PASSWORD"'")'
+} >> /github/home/.sbt/1.0/global.sbt
+sha1sum /github/home/.sbt/1.0/global.sbt
 
 touch /github/home/.private.key
 cat /dev/null > /github/home/.private.key
